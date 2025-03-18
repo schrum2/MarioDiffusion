5005b1689a4ea551a84575b5725bcad1d70e0740
Jacob Schrum <schrum2@southwestern.edu>
2025-03-17 21:26:17 -0500

    Change upper/lower boundaries again
    #2

--- boundaries.py
+++ boundaries.py
@@ -1,6 +1,8 @@
-
-TOP_LINE = 256 / 3
-BOTTOM_LINE = 256 - (256 / 3)
+# 224 is the highest height of the original levels before the sky buffer is added
+# Nothing of interest can appear above this line, which is why the height of interest
+# is lowered.
+TOP_LINE = 32 + 224 / 3
+BOTTOM_LINE = 256 - (224 / 3)
 
 LEFT_LINE = 256 / 3
 RIGHT_LINE = 256 - (256 / 3)
