5518cb5293f6dfd79a794e43045677e497d54eae
Jacob Schrum <schrum2@southwestern.edu>
2025-04-13 15:14:12 -0500

    Suggested changes to model from Claude
    #21

--- wgan_model.py
+++ wgan_model.py
@@ -2,100 +2,131 @@ import torch
 import torch.nn as nn
 import torch.nn.parallel
 
-class DCGAN_D(nn.Module):
-    def __init__(self, isize, nz, nc, ndf, ngpu, n_extra_layers=0):
-        super(DCGAN_D, self).__init__()
+class WGAN_Discriminator(nn.Module):
+    def __init__(self, isize, nc, ndf, ngpu=1, n_extra_layers=0):
+        """
+        WGAN Discriminator (Critic) model
+        
+        Args:
+            isize: Input image size (width/height), must be a multiple of 16
+            nc: Number of input channels (tile types)
+            ndf: Size of feature maps in discriminator
+            ngpu: Number of GPUs
+            n_extra_layers: Number of extra conv layers
+        """
+        super(WGAN_Discriminator, self).__init__()
         self.ngpu = ngpu
         assert isize % 16 == 0, "isize has to be a multiple of 16"
 
         main = nn.Sequential()
-        # input is nc x isize x isize
-        main.add_module('initial:conv:{0}-{1}'.format(nc, ndf),
+        # Input is nc (tile types) x isize x isize
+        main.add_module('initial-conv',
                         nn.Conv2d(nc, ndf, 4, 2, 1, bias=False))
-        main.add_module('initial:relu:{0}'.format(ndf),
+        main.add_module('initial-lrelu',
                         nn.LeakyReLU(0.2, inplace=True))
-        csize, cndf = isize / 2, ndf
+        
+        csize, cndf = isize // 2, ndf
 
         # Extra layers
         for t in range(n_extra_layers):
-            main.add_module('extra-layers-{0}:{1}:conv'.format(t, cndf),
+            main.add_module(f'extra-{t}-conv',
                             nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
-            main.add_module('extra-layers-{0}:{1}:batchnorm'.format(t, cndf),
+            main.add_module(f'extra-{t}-batchnorm',
                             nn.BatchNorm2d(cndf))
-            main.add_module('extra-layers-{0}:{1}:relu'.format(t, cndf),
+            main.add_module(f'extra-{t}-lrelu',
                             nn.LeakyReLU(0.2, inplace=True))
 
+        # Downsampling layers
         while csize > 4:
             in_feat = cndf
             out_feat = cndf * 2
-            main.add_module('pyramid:{0}-{1}:conv'.format(in_feat, out_feat),
+            main.add_module(f'down-{in_feat}-{out_feat}-conv',
                             nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
-            main.add_module('pyramid:{0}:batchnorm'.format(out_feat),
+            main.add_module(f'down-{out_feat}-batchnorm',
                             nn.BatchNorm2d(out_feat))
-            main.add_module('pyramid:{0}:relu'.format(out_feat),
+            main.add_module(f'down-{out_feat}-lrelu',
                             nn.LeakyReLU(0.2, inplace=True))
-            cndf = cndf * 2
-            csize = csize / 2
+            cndf = out_feat
+            csize = csize // 2
 
-        # state size. K x 4 x 4
-        main.add_module('final:{0}-{1}:conv'.format(cndf, 1),
+        # Final layer to single value output
+        main.add_module('final-conv',
                         nn.Conv2d(cndf, 1, 4, 1, 0, bias=False))
+        
         self.main = main
-
 
     def forward(self, input):
         if self.ngpu > 1 and isinstance(input.data, torch.cuda.FloatTensor):
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
         else: 
             output = self.main(input)
-            
+        
+        # Average across the batch dimension
         output = output.mean(0)
         return output.view(1)
 
-class DCGAN_G(nn.Module):
-    def __init__(self, isize, nz, nc, ngf, ngpu, n_extra_layers=0):
-        super(DCGAN_G, self).__init__()
+
+class WGAN_Generator(nn.Module):
+    def __init__(self, isize, nz, nc, ngf, ngpu=1, n_extra_layers=0):
+        """
+        WGAN Generator model
+        
+        Args:
+            isize: Output image size (width/height), must be a multiple of 16
+            nz: Size of latent vector
+            nc: Number of output channels (tile types)
+            ngf: Size of feature maps in generator
+            ngpu: Number of GPUs
+            n_extra_layers: Number of extra conv layers
+        """
+        super(WGAN_Generator, self).__init__()
         self.ngpu = ngpu
         assert isize % 16 == 0, "isize has to be a multiple of 16"
 
+        # Calculate initial feature map size
         cngf, tisize = ngf//2, 4
         while tisize != isize:
             cngf = cngf * 2
             tisize = tisize * 2
 
         main = nn.Sequential()
-        # input is Z, going into a convolution
-        main.add_module('initial:{0}-{1}:convt'.format(nz, cngf),
+        # Input is latent vector Z
+        main.add_module('initial-convt',
                         nn.ConvTranspose2d(nz, cngf, 4, 1, 0, bias=False))
-        main.add_module('initial:{0}:batchnorm'.format(cngf),
+        main.add_module('initial-batchnorm',
                         nn.BatchNorm2d(cngf))
-        main.add_module('initial:{0}:relu'.format(cngf),
+        main.add_module('initial-relu',
                         nn.ReLU(True))
 
         csize, cndf = 4, cngf
+        # Upsampling layers
         while csize < isize//2:
-            main.add_module('pyramid:{0}-{1}:convt'.format(cngf, cngf//2),
+            main.add_module(f'up-{cngf}-{cngf//2}-convt',
                             nn.ConvTranspose2d(cngf, cngf//2, 4, 2, 1, bias=False))
-            main.add_module('pyramid:{0}:batchnorm'.format(cngf//2),
+            main.add_module(f'up-{cngf//2}-batchnorm',
                             nn.BatchNorm2d(cngf//2))
-            main.add_module('pyramid:{0}:relu'.format(cngf//2),
+            main.add_module(f'up-{cngf//2}-relu',
                             nn.ReLU(True))
             cngf = cngf // 2
             csize = csize * 2
 
         # Extra layers
         for t in range(n_extra_layers):
-            main.add_module('extra-layers-{0}:{1}:conv'.format(t, cngf),
+            main.add_module(f'extra-{t}-conv',
                             nn.Conv2d(cngf, cngf, 3, 1, 1, bias=False))
-            main.add_module('extra-layers-{0}:{1}:batchnorm'.format(t, cngf),
+            main.add_module(f'extra-{t}-batchnorm',
                             nn.BatchNorm2d(cngf))
-            main.add_module('extra-layers-{0}:{1}:relu'.format(t, cngf),
+            main.add_module(f'extra-{t}-relu',
                             nn.ReLU(True))
 
-        main.add_module('final:{0}-{1}:convt'.format(cngf, nc),
+        # Final layer to output
+        main.add_module('final-convt',
                         nn.ConvTranspose2d(cngf, nc, 4, 2, 1, bias=False))
-        main.add_module('final:{0}:tanh'.format(nc),
-                        nn.ReLU())#nn.Softmax(1))    #Was TANH nn.Tanh())#
+        
+        # We use softmax for one-hot encoding output
+        main.add_module('final-activation',
+                        nn.Softmax(dim=1))
+        
         self.main = main
 
     def forward(self, input):
@@ -103,100 +134,4 @@ class DCGAN_G(nn.Module):
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
         else: 
             output = self.main(input)
-
-        #print (output[0,:,0,0])
-        #exit()
-        return output 
-###############################################################################
-class DCGAN_D_nobn(nn.Module):
-    def __init__(self, isize, nz, nc, ndf, ngpu, n_extra_layers=0):
-        super(DCGAN_D_nobn, self).__init__()
-        self.ngpu = ngpu
-        assert isize % 16 == 0, "isize has to be a multiple of 16"
-
-        main = nn.Sequential()
-        # input is nc x isize x isize
-        # input is nc x isize x isize
-        main.add_module('initial:conv:{0}-{1}'.format(nc, ndf),
-                        nn.Conv2d(nc, ndf, 4, 2, 1, bias=False))
-        main.add_module('initial:relu:{0}'.format(ndf),
-                        nn.LeakyReLU(0.2, inplace=True))
-        csize, cndf = isize / 2, ndf
-
-        # Extra layers
-        for t in range(n_extra_layers):
-            main.add_module('extra-layers-{0}:{1}:conv'.format(t, cndf),
-                            nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
-            main.add_module('extra-layers-{0}:{1}:relu'.format(t, cndf),
-                            nn.LeakyReLU(0.2, inplace=True))
-
-        while csize > 4:
-            in_feat = cndf
-            out_feat = cndf * 2
-            main.add_module('pyramid:{0}-{1}:conv'.format(in_feat, out_feat),
-                            nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
-            main.add_module('pyramid:{0}:relu'.format(out_feat),
-                            nn.LeakyReLU(0.2, inplace=True))
-            cndf = cndf * 2
-            csize = csize / 2
-
-        # state size. K x 4 x 4
-        main.add_module('final:{0}-{1}:conv'.format(cndf, 1),
-                        nn.Conv2d(cndf, 1, 4, 1, 0, bias=False))
-        self.main = main
-
-
-    def forward(self, input):
-        if self.ngpu > 1 and isinstance(input.data, torch.cuda.FloatTensor):
-            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-        else: 
-            output = self.main(input)
-            
-        output = output.mean(0)
-        return output.view(1)
-
-class DCGAN_G_nobn(nn.Module):
-    def __init__(self, isize, nz, nc, ngf, ngpu, n_extra_layers=0):
-        super(DCGAN_G_nobn, self).__init__()
-        self.ngpu = ngpu
-        assert isize % 16 == 0, "isize has to be a multiple of 16"
-
-        cngf, tisize = ngf//2, 4
-        while tisize != isize:
-            cngf = cngf * 2
-            tisize = tisize * 2
-
-        main = nn.Sequential()
-        main.add_module('initial:{0}-{1}:convt'.format(nz, cngf),
-                        nn.ConvTranspose2d(nz, cngf, 4, 1, 0, bias=False))
-        main.add_module('initial:{0}:relu'.format(cngf),
-                        nn.ReLU(True))
-
-        csize, cndf = 4, cngf
-        while csize < isize//2:
-            main.add_module('pyramid:{0}-{1}:convt'.format(cngf, cngf//2),
-                            nn.ConvTranspose2d(cngf, cngf//2, 4, 2, 1, bias=False))
-            main.add_module('pyramid:{0}:relu'.format(cngf//2),
-                            nn.ReLU(True))
-            cngf = cngf // 2
-            csize = csize * 2
-
-        # Extra layers
-        for t in range(n_extra_layers):
-            main.add_module('extra-layers-{0}:{1}:conv'.format(t, cngf),
-                            nn.Conv2d(cngf, cngf, 3, 1, 1, bias=False))
-            main.add_module('extra-layers-{0}:{1}:relu'.format(t, cngf),
-                            nn.ReLU(True))
-
-        main.add_module('final:{0}-{1}:convt'.format(cngf, nc),
-                        nn.ConvTranspose2d(cngf, nc, 4, 2, 1, bias=False))
-        main.add_module('final:{0}:tanh'.format(nc),
-                        nn.Softmax())#Tanh())
-        self.main = main
-
-    def forward(self, input):
-        if self.ngpu > 1 and isinstance(input.data, torch.cuda.FloatTensor):
-            output = nn.parallel.data_parallel(self.main, input,  range(self.ngpu))
-        else: 
-            output = self.main(input)
-        return output 
+        return output
