3bb2518bab9d978562c1407bc5be003a2fc21878
Jacob Schrum <schrum2@southwestern.edu>
2025-04-14 07:57:30 -0500

    Batch for gan model train sequence
    #21 Need to sample at the end.

---
+++ wgan_model_sequence.bat
@@ -0,0 +1,4 @@
+python create_level_json_data.py --output "SMB1_Levels.json"
+python create_ascii_captions.py --dataset SMB1_Levels.json --output SMB1_LevelsAndCaptions.json
+python tokenizer.py save
+python train_wgan.py --augment
