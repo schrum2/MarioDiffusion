a341b6f15c47df8675c807072e7f95d3edc4f49e
Jacob Schrum <schrum2@southwestern.edu>
2025-04-15 16:40:00 -0500

    evaluate at end

--- conditional_model_sequence.bat
+++ conditional_model_sequence.bat
@@ -4,3 +4,4 @@ python tokenizer.py save
 python train_mlm.py --epochs 300
 python train_diffusion.py --augment --text_conditional --output_dir "conditional-model" --num_epochs 300
 python run_diffusion.py --model_path conditional-model --num_samples 100 --text_conditional --save_as_json --output_dir "conditional_model_unconditional_samples"
+python evaluate_caption_adherence.py --model_path conditional-model --save_as_json
