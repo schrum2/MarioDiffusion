c557e0079a10ef3392260472f852911f71fc0980
Jacob Schrum <schrum2@southwestern.edu>
2025-04-17 16:51:22 -0500

    Working on evolving in GAN latent space
    #21

---
+++ evolve_wgan.py
@@ -0,0 +1,97 @@
+from evolution import Evolver
+from level_dataset import visualize_samples, convert_to_level_format
+from caption_match import compare_captions
+from create_ascii_captions import assign_caption, extract_tileset
+import numpy as np
+import argparse
+import torch
+from wgan_model import WGAN_Generator
+from run_wgan import generate_level_scene_from_latent
+
+from genome import DiffusionGenome
+
+class WGANEvolver(Evolver):
+    def __init__(self, args):
+        Evolver.__init__(self)
+
+        self.args = args
+
+        self.width = args.width
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Set input image size (16x16 for your level data)
+        isize = 16
+        self.netG = WGAN_Generator(isize, args.nz, args.num_tiles, args.ngf, n_extra_layers=args.n_extra_layers)
+
+        # Load trained model
+        try:
+            netG.load_state_dict(torch.load(args.model_path, map_location=device))
+            print(f"Successfully loaded generator model from {args.model_path}")
+        except Exception as e:
+            print(f"Error loading model: {e}")
+            raise ValueError(f"failed {args.model_path}")
+
+        # Move model to device and set to evaluation mode
+        self.netG = self.netG.to(self.device)
+        self.netG.eval()
+
+        _, self.id_to_char, self.char_to_id, self.tile_descriptors = extract_tileset(args.tileset_path)
+
+    def random_latent(self, seed=1, batch_size=1):
+        # Generate random noise
+        noise = torch.randn(batch_size, self.args.nz, 1, 1, device="cpu") # On CPU since population evolves in memory
+        return noise
+
+    def initialize_population(self):
+        self.genomes = [DiffusionGenome(self.width, seed, self.steps, self.guidance_scale, latents=self.random_latent(seed)) for seed in range(self.population_size)]
+        self.viewer.id_to_char = self.id_to_char
+
+    def generate_image(self, g):
+        # generate fresh new image
+        print(f"Generate new image for {g}")
+
+        noise = g.latents.to(self.device)
+        samples_cpu = generate_level_scene_from_latent(self.netG, noise)
+        g.latents.to("cpu")
+
+        print(samples_cpu)
+        print(samples_cpu.shape)
+        sample_indices = convert_to_level_format(samples_cpu)
+        print(sample_indices)
+        
+        # Add level data to the list
+        scene = sample_indices[0].tolist() # Always just one scene: (1,16,16)
+        #print(scene)
+        g.scene = scene 
+
+        # actual_caption = assign_caption(scene, self.id_to_char, self.char_to_id, self.tile_descriptors, False, False) # self.args.describe_locations, self.args.describe_absence)
+
+        #print(f"Describe resulting image: {actual_caption}")
+        #compare_score = compare_captions(self.prompt, actual_caption)
+        #print(f"Comparison score: {compare_score}")
+
+        return visualize_samples(samples_cpu)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Evolve levels with WGAN")    
+
+    parser.add_argument("--model_path", type=str, required=True, help="Path to the trained diffusion model")
+    parser.add_argument("--tileset_path", default='..\TheVGLC\Super Mario Bros\smb.json', help="Descriptions of individual tile types")
+    parser.add_argument("--describe_locations", action="store_true", default=False, help="Include location descriptions in the captions")
+    parser.add_argument("--describe_absence", action="store_true", default=False, help="Indicate when there are no occurrences of an item or structure")
+    parser.add_argument("--width", type=int, default=16, help="Tile width of generated level")
+    parser.add_argument("--num_tiles", type=int, default=15, help="Number of tile types")
+    
+
+    parser.add_argument("--num_samples", type=int, default=10, help="Number of samples to generate")
+    ### parser.add_argument("--batch_size", type=int, default=10, help="Batch size for generation")
+    parser.add_argument("--nz", type=int, default=32, help="Size of the latent z vector")
+    parser.add_argument("--ngf", type=int, default=64, help="Size of feature maps in generator")
+    parser.add_argument("--n_extra_layers", type=int, default=0, help="Number of extra layers in generator")
+    parser.add_argument("--seed", type=int, default=None, help="Random seed for reproducibility")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    evolver = WGANEvolver(args)
+    evolver.start_evolution()
