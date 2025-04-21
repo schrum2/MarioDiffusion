e532c2df912533bfd451502961f8b7c6b668056b
Jacob Schrum <schrum2@southwestern.edu>
2025-04-21 10:59:14 -0500

    Tokenizer needed before validation set made
    There is probably a way around this, but it's easier just to change the order

--- conditional_model_sequence.bat
+++ conditional_model_sequence.bat
@@ -1,8 +1,8 @@
 python create_level_json_data.py --output "SMB1_Levels.json"
 python create_ascii_captions.py --dataset SMB1_Levels.json --output SMB1_LevelsAndCaptions.json
-python validation_captions.py --save_file "SMB1_ValidationCaptions.json"
 python tokenizer.py save
 python train_mlm.py --epochs 300
+python validation_captions.py --save_file "SMB1_ValidationCaptions.json"
 python train_diffusion.py --augment --text_conditional --output_dir "conditional-model" --num_epochs 200
 python run_diffusion.py --model_path conditional-model --num_samples 100 --text_conditional --save_as_json --output_dir "conditional_model_unconditional_samples"
 python evaluate_caption_adherence.py --model_path conditional-model --save_as_json
