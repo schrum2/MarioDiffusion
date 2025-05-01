2c14212cdbdb433455fca578d575a290a86f5ce6
Jacob Schrum <schrum2@southwestern.edu>
2025-05-01 08:39:29 -0500

    Better named for output directories

--- conditional_model_sequence.bat
+++ conditional_model_sequence.bat
@@ -4,7 +4,7 @@ python tokenizer.py save
 python train_mlm.py --epochs 300
 python create_validation_captions.py --save_file "SMB1_ValidationCaptions.json"
 python train_diffusion.py --augment --text_conditional --output_dir "conditional-model" --num_epochs 200
-python run_diffusion.py --model_path conditional-model --num_samples 100 --text_conditional --save_as_json --output_dir "conditional_model_unconditional_samples"
-python evaluate_caption_adherence.py --model_path conditional-model --save_as_json
-python evaluate_caption_adherence.py --model_path conditional-model --save_as_json --compare_checkpoints --output_dir text_to_level_training
-python evaluate_caption_adherence.py --model_path conditional-model --save_as_json --compare_checkpoints --json SMB1_ValidationCaptions.json --output_dir text_to_level_validation
+python run_diffusion.py --model_path conditional-model --num_samples 100 --text_conditional --save_as_json --output_dir "conditional-model-unconditional-samples"
+python evaluate_caption_adherence.py --model_path conditional-model --save_as_json --output_dir conditional-model-text-to-level-final
+python evaluate_caption_adherence.py --model_path conditional-model --save_as_json --compare_checkpoints --output_dir conditional-model-text-to-level-training
+python evaluate_caption_adherence.py --model_path conditional-model --save_as_json --compare_checkpoints --json SMB1_ValidationCaptions.json --output_dir conditional-model-text-to-level-validation
