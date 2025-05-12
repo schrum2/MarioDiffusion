42c95bca29622c986ece8a5de19b51dcfceda06c
Jacob Schrum <schrum2@southwestern.edu>
2025-05-12 10:35:29 -0500

    naming convention
    #44

--- conditional_model_sequence-regular.bat
+++ conditional_model_sequence-regular.bat
@@ -2,14 +2,14 @@ python create_level_json_data.py --output "SMB1_Levels.json" --levels "..\\TheVG
 python create_level_json_data.py --output "SMB2_Levels.json" --levels "..\\TheVGLC\\Super Mario Bros 2 (Japan)\\Processed"
 python create_level_json_data.py --output "SML_Levels.json" --levels "..\\TheVGLC\\Super Mario Land\\Processed"
 python combine_data.py Mario_Levels.json SMB1_Levels.json SMB2_Levels.json SML_Levels.json
-python create_ascii_captions.py --dataset Mario_Levels.json --output Mario_LevelsAndCaptions.json
-python tokenizer.py save --json_file Mario_LevelsAndCaptions.json --pkl_file Mario_Tokenizer.pkl
-python train_mlm.py --epochs 300 --save_checkpoints --json Mario_LevelsAndCaptions.json --pkl Mario_Tokenizer.pkl --output_dir mlm
-python create_validation_captions.py --save_file "Mario_ValidationCaptions.json" --pkl Mario_Tokenizer.pkl --json Mario_LevelsAndCaptions.json  --seed 0
-python evaluate_masked_token_prediction.py --model_path mlm --compare_checkpoints --json Mario_LevelsAndCaptions.json
-python evaluate_masked_token_prediction.py --model_path mlm --compare_checkpoints --json Mario_ValidationCaptions.json
-python train_diffusion.py --augment --text_conditional --output_dir "Mario-conditional-model" --num_epochs 200 --json Mario_LevelsAndCaptions.json --pkl Mario_Tokenizer.pkl
-python run_diffusion.py --model_path Mario-conditional-model --num_samples 100 --text_conditional --save_as_json --output_dir "Mario-conditional-model-unconditional-samples"
-python evaluate_caption_adherence.py --model_path Mario-conditional-model --save_as_json --json Mario_LevelsAndCaptions.json --output_dir conditional-model-text-to-level-final
-python evaluate_caption_adherence.py --model_path Mario-conditional-model --save_as_json --json Mario_LevelsAndCaptions.json --compare_checkpoints 
-python evaluate_caption_adherence.py --model_path Mario-conditional-model --save_as_json --json Mario_ValidationCaptions.json --compare_checkpoints 
+python create_ascii_captions.py --dataset Mario_Levels.json --output Mario_LevelsAndCaptions-regular.json
+python tokenizer.py save --json_file Mario_LevelsAndCaptions-regular.json --pkl_file Mario_Tokenizer-regular.pkl
+python train_mlm.py --epochs 300 --save_checkpoints --json Mario_LevelsAndCaptions-regular.json --pkl Mario_Tokenizer-regular.pkl --output_dir mlm-regular
+python create_validation_captions.py --save_file "Mario_ValidationCaptions-regular.json" --pkl Mario_Tokenizer-regular.pkl --json Mario_LevelsAndCaptions-regular.json  --seed 0
+python evaluate_masked_token_prediction.py --model_path mlm-regular --compare_checkpoints --json Mario_LevelsAndCaptions-regular.json
+python evaluate_masked_token_prediction.py --model_path mlm-regular --compare_checkpoints --json Mario_ValidationCaptions-regular.json
+python train_diffusion.py --augment --text_conditional --output_dir "Mario-conditional-model-regular" --num_epochs 200 --json Mario_LevelsAndCaptions-regular.json --pkl Mario_Tokenizer-regular.pkl
+python run_diffusion.py --model_path Mario-conditional-model-regular --num_samples 100 --text_conditional --save_as_json --output_dir "Mario-conditional-model-regular-unconditional-samples"
+python evaluate_caption_adherence.py --model_path Mario-conditional-model-regular --save_as_json --json Mario_LevelsAndCaptions-regular.json --output_dir conditional-model-text-to-level-final
+python evaluate_caption_adherence.py --model_path Mario-conditional-model-regular --save_as_json --json Mario_LevelsAndCaptions-regular.json --compare_checkpoints 
+python evaluate_caption_adherence.py --model_path Mario-conditional-model-regular --save_as_json --json Mario_ValidationCaptions-regular.json --compare_checkpoints 
