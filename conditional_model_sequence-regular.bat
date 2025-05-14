814f03f1d17ba82e7b15f2595dd4b6672f274deb
Jacob Schrum <schrum2@southwestern.edu>
2025-05-14 10:17:05 -0500

    Use the train,validate,test split
    #45

--- conditional_model_sequence-regular.bat
+++ conditional_model_sequence-regular.bat
@@ -4,7 +4,7 @@ python create_level_json_data.py --output "SML_Levels.json" --levels "..\\TheVGL
 python combine_data.py Mario_Levels.json SMB1_Levels.json SMB2_Levels.json SML_Levels.json
 python create_ascii_captions.py --dataset Mario_Levels.json --output Mario_LevelsAndCaptions-regular.json
 python tokenizer.py save --json_file Mario_LevelsAndCaptions-regular.json --pkl_file Mario_Tokenizer-regular.pkl
-python train_mlm.py --epochs 300 --save_checkpoints --json Mario_LevelsAndCaptions-regular.json --pkl Mario_Tokenizer-regular.pkl --output_dir mlm-regular
+python train_mlm.py --epochs 300 --save_checkpoints --json Mario_LevelsAndCaptions-regular.json --pkl Mario_Tokenizer-regular.pkl --output_dir mlm-regular --split
 python create_validation_captions.py --save_file "Mario_ValidationCaptions-regular.json" --pkl Mario_Tokenizer-regular.pkl --json Mario_LevelsAndCaptions-regular.json  --seed 0
 python evaluate_masked_token_prediction.py --model_path mlm-regular --compare_checkpoints --json Mario_LevelsAndCaptions-regular.json
 python evaluate_masked_token_prediction.py --model_path mlm-regular --compare_checkpoints --json Mario_ValidationCaptions-regular.json
