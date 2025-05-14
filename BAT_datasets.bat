a5db9c818569dcd2982ca23eb3ce6e4380af0b61
Jacob Schrum <schrum2@southwestern.edu>
2025-05-14 16:06:43 -0500

    Make validation sets too
    May want to change the name to something other than validation #45
    
    Also, caption creator no longer needs tokenizer

--- BAT_datasets.bat
+++ BAT_datasets.bat
@@ -29,7 +29,5 @@ python tokenizer.py save --json_file Mario_LevelsAndCaptions-absence.json    --p
 python tokenizer.py save --json_file SMB1AND2_LevelsAndCaptions-regular.json --pkl_file SMB1AND2_Tokenizer-regular.pkl
 python tokenizer.py save --json_file SMB1AND2_LevelsAndCaptions-absence.json --pkl_file SMB1AND2_Tokenizer-absence.pkl
 
-
-
-
-python create_validation_captions.py --save_file "Mario_ValidationCaptions-regular.json" --pkl Mario_Tokenizer-regular.pkl --json Mario_LevelsAndCaptions-regular.json  --seed 0
+python create_validation_captions.py --save_file "Mario_ValidationCaptions-regular.json" --json Mario_LevelsAndCaptions-regular.json --seed 0
+python create_validation_captions.py --save_file "Mario_ValidationCaptions-absence.json" --json Mario_LevelsAndCaptions-absence.json --seed 0 --describe_absence
