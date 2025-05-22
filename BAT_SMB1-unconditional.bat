cc76829653bfc36b9e29c292897b8104858c51eb
Bess Hagan <142757968+besshagan@users.noreply.github.com>
2025-05-22 16:50:19 -0500

    Adjusted batch files: removed --split, added --val_json and adjusted file paths to end with either -train or -validate
    #45

--- BAT_SMB1-unconditional.bat
+++ BAT_SMB1-unconditional.bat
@@ -1,2 +1,2 @@
-python train_diffusion.py --augment --output_dir "SMB1-unconditional" --num_epochs 100 --json SMB1_LevelsAndCaptions-regular.json --split
+python train_diffusion.py --augment --output_dir "SMB1-unconditional" --num_epochs 100 --json SMB1_LevelsAndCaptions-regular-train.json --val_json SMB1_LevelsAndCaptions-regular-validate.json
 python run_diffusion.py --model_path SMB1-unconditional --num_samples 100 --save_as_json --output_dir "SMB1-unconditional-samples"
