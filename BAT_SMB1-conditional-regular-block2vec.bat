6916e25ef99f029251b1eb35bf2acc0b71cfb4b2
salasemilio1 <salas2@southwestern.edu>
2025-05-23 10:39:33 -0500

    Issue with hugging face save/load pretrain functionality
    #37 There is an error with the hugging face convention functionality: Repository Not Found for url: https://huggingface.co/api/models/SMB1-conditional-block2vec.

--- BAT_SMB1-conditional-regular-block2vec.bat
+++ BAT_SMB1-conditional-regular-block2vec.bat
@@ -1,15 +1,14 @@
 python create_tile_level_json_data.py --output "SMB1_3x3_tiles.json" --tile_size 3
-python train_block2vec.py --json_file "SMB1_3x3_tiles.json" --output_dir "SMB1-regular-block2vec-embeddings" --embedding_dim 16 --epochs 300 --batch_size 32
+python train_block2vec.py --json_file "SMB1_3x3_tiles.json" --output_dir "SMB1-regular-block2vec-embeddings" --embedding_dim 16 --epochs 20 --batch_size 32
 python train_diffusion.py ^
     --augment ^
     --text_conditional ^
     --output_dir "SMB1-conditional-block2vec" ^
-    --num_epochs 100 ^
+    --num_epochs 20^
     --json "SMB1_LevelsAndCaptions-regular.json" ^
     --pkl "SMB1_Tokenizer-regular.pkl" ^
     --mlm_model_dir "SMB1-regular-block2vec-embeddings" ^
     --block_embedding_model_path "SMB1-regular-block2vec-embeddings" ^
-    --split ^
     --plot_validation_caption_score
 python run_diffusion.py ^
     --model_path "SMB1-conditional-block2vec" ^
