59083c2f278747e341218660f326d931966c7514
Bess Hagan <142757968+besshagan@users.noreply.github.com>
2025-05-23 16:36:33 -0500

    Adjustments made to BAT file to access data that is split prior to training. train_mlm line will need further adjustment after the script is altered to remove split arg.
    #70

--- BAT_SMB1-conditional-regular.bat
+++
@@ -1,7 +0,0 @@
-python train_mlm.py --epochs 300 --save_checkpoints --json SMB1_LevelsAndCaptions-regular.json --pkl SMB1_Tokenizer-regular.pkl --output_dir SMB1-MLM-regular --split
-python split_data.py --json SMB1_LevelsAndCaptions-regular.json --train_pct 0.9 --val_pct 0.05 --test_pct 0.05 --seed 42
-python train_diffusion.py --augment --text_conditional --output_dir "SMB1-conditional-regular" --num_epochs 100 --json SMB1_LevelsAndCaptions-regular-train.json --val_json SMB1_LevelsAndCaptions-regular-validate.json --pkl SMB1_Tokenizer-regular.pkl --mlm_model_dir SMB1-MLM-regular --plot_validation_caption_score
-python run_diffusion.py --model_path SMB1-conditional-regular --num_samples 100 --text_conditional --save_as_json --output_dir "SMB1-conditional-regular-unconditional-samples"
-python evaluate_caption_adherence.py --model_path SMB1-conditional-regular --save_as_json --json SMB1_LevelsAndCaptions-regular.json --output_dir text-to-level-final
-python evaluate_caption_adherence.py --model_path SMB1-conditional-regular --save_as_json --json SMB1_LevelsAndCaptions-regular.json --compare_checkpoints 
-python evaluate_caption_adherence.py --model_path SMB1-conditional-regular --save_as_json --json SMB1_ValidationCaptions-regular.json --compare_checkpoints 
