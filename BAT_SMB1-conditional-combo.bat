4f7c1d7e01c480fffde80721ee0d7803e80a7a01
Bess Hagan <142757968+besshagan@users.noreply.github.com>
2025-05-23 10:03:37 -0500

    Created new BAT file to run long conditional training with COMBO loss functions.
    #57

---
+++ BAT_SMB1-conditional-combo.bat
@@ -0,0 +1,3 @@
+python train_diffusion.py --augment --text_conditional --output_dir "SMB1-conditional-regular-combo" --num_epochs 500 --json SMB1_LevelsAndCaptions-regular-train.json --val_json SMB1_LevelsAndCaptions-regular-validate.json --pkl SMB1_Tokenizer-regular.pkl --mlm_model_dir SMB1-MLM-regular --plot_validation_caption_score --loss_type COMBO
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+python run_diffusion.py --model_path SMB1-conditional-regular-combo --num_samples 100 --save_as_json --output_dir "SMB1-conditional-regular-combo-samples"
