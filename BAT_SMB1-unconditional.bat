748be3e68abff648257e427d8338feb0aef724db
Jacob Schrum <schrum2@southwestern.edu>
2025-05-24 14:37:39 -0500

    Moved/changed unconditional batch file
    #70 #45

--- BAT_SMB1-unconditional.bat
+++
@@ -1,3 +0,0 @@
-python split_data.py --json SMB1_LevelsAndCaptions-regular.json --train_pct 0.9 --val_pct 0.05 --test_pct 0.05 --seed 42
-python train_diffusion.py --augment --output_dir "SMB1-unconditional" --num_epochs 100 --json SMB1_LevelsAndCaptions-regular-train.json --val_json SMB1_LevelsAndCaptions-regular-validate.json
-python run_diffusion.py --model_path SMB1-unconditional --num_samples 100 --save_as_json --output_dir "SMB1-unconditional-samples"
