ee69e05f6bc63b211a339307f87845d631346edf
Jacob Schrum <schrum2@southwestern.edu>
2025-05-26 23:20:09 -0500

    Moved gte batch file
    @Olivia-k18 Moved the gte batch file as well for issue #70 like I did for the MiniLM file, though I didn't test this batch file after moving the way I did the other.

--- batch/SMB1-conditional-regular-MiniLM.bat
+++ batch/SMB1-conditional-regular-MiniLM.bat
@@ -1,4 +1,3 @@
-
 cd ..
 
 python train_diffusion.py --augment --text_conditional --output_dir "SMB1-conditional-regular-MiniLM" --num_epochs 500 --json datasets\\SMB1_LevelsAndCaptions-regular-train.json --val_json datasets\\SMB1_LevelsAndCaptions-regular-validate.json --pretrained_language_model "sentence-transformers/multi-qa-MiniLM-L6-cos-v1" --plot_validation_caption_score
