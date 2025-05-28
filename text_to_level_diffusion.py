f48e995e777df5480a90ebeb252d22bd058ce295
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-05-28 09:57:44 -0500

    Added common settings
    #81
    Added common settings for Mario and Lode Runner

--- text_to_level_diffusion.py
+++ text_to_level_diffusion.py
@@ -8,6 +8,7 @@ from LR_create_ascii_captions import assign_caption as lr_assign_caption
 from captions.util import extract_tileset
 import argparse
 import util.common_settings as common_settings
+import util.LR_common_settings as lr_common_settings
 import sys
 import os
 
@@ -82,9 +83,10 @@ class InteractiveLevelGeneration(InteractiveGeneration):
         # Assign a caption to the sceneof whichever game is being played
         if self.args.game == "Mario":
             actual_caption = assign_caption(scene, self.id_to_char, self.char_to_id, self.tile_descriptors, False, self.args.describe_absence)
-
+            level_width = common_settings.MARIO_WIDTH
         elif self.args.game == "LR":
             actual_caption = lr_assign_caption(scene, self.id_to_char, self.char_to_id, self.tile_descriptors, False, self.args.describe_absence)
+            level_width = lr_common_settings.LR_WIDTH
         else:
             raise ValueError(f"Unknown game: {self.args.game}")
         
@@ -95,7 +97,7 @@ class InteractiveLevelGeneration(InteractiveGeneration):
         # Use the new function to process scene segments
         average_score, segment_captions, segment_scores = process_scene_segments(
             scene=scene,
-            segment_width=common_settings.MARIO_WIDTH,
+            segment_width=level_width,
             prompt=param_values.get("caption", ""),
             id_to_char=self.id_to_char,
             char_to_id=self.char_to_id,
@@ -126,10 +128,10 @@ if __name__ == "__main__":
         width = common_settings.MARIO_WIDTH
         args.tileset = '..\TheVGLC\Super Mario Bros\smb.json'
     elif args.game == "LR":
-        args.num_tiles = 10 
-        height = 32
-        width = 32
-        args.tileset = '..\TheVGLC\Lode Runner\Loderunner.json' 
+        args.num_tiles = lr_common_settings.LR_TILE_COUNT
+        height = lr_common_settings.LR_HEIGHT
+        width = lr_common_settings.LR_WIDTH
+        args.tileset = '..\TheVGLC\Lode Runner\Loderunner.json'
     else:
         raise ValueError(f"Unknown game: {args.game}")
     
