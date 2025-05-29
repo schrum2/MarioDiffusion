e8dd912396ef9fbbd3921d87d5eefe41369c4171
Bess Hagan <142757968+besshagan@users.noreply.github.com>
2025-05-29 13:46:34 -0500

    rennamed files for training and running MarioGPT
    #52

--- train_gpt2.py
+++ train_gpt2.py
@@ -28,28 +28,26 @@ train_config = TrainingConfig(save_iteration=10)
 trainer = MarioGPTTrainer(mario_lm, dataset, config=train_config)
 print("Trainer ready.")
 
-# Inference before training
-print("Generating a sample level before training...")
-prompts = ["many pipes, many enemies, some blocks, high elevation"]
+# # Inference before training
+# print("Generating a sample level before training...")
+# prompts = ["many pipes, many enemies, some blocks, high elevation"]
 
-# generate level of size 1400, pump temperature up to ~2.4 for more stochastic but playable levels
-generated_level = mario_lm.sample(
-    prompts=prompts,
-    num_steps=1400,
-    temperature=2.0,
-    use_tqdm=True
-)
+# # generate level of size 1400, pump temperature up to ~2.4 for more stochastic but playable levels
+# generated_level = mario_lm.sample(
+#     prompts=prompts,
+#     num_steps=1400,
+#     temperature=2.0,
+#     use_tqdm=True
+# )
 
-# Show sample
-print("Generated level (string representation):")
-print(generated_level.level)
-print("Generated level (PIL image):")
-generated_level.img.show()
-print("Pretraining sample level generation complete.")
+# # Show sample
+# print("Generated level (string representation):")
+# print(generated_level.level)
+# print("Generated level (PIL image):")
+# generated_level.img.show()
+# print("Pretraining sample level generation complete.")
 
 print("Starting training for 100 iterations...")
 # train for 100 iterations!
-trainer.train(100, batch_size=1)
+trainer.train(50000, batch_size=1)
 print("Training complete!")
-
-
