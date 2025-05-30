f227353f0aa81ee1128ac81466a5fdb7039d09c4
Jacob Schrum <schrum2@southwestern.edu>
2025-05-30 08:55:17 -0500

    Just forgot to remove parts of this

--- batch/SMB1-unconditional.bat
+++ batch/SMB1-unconditional.bat
@@ -1,11 +1,4 @@
-cd ..
-
-python train_diffusion.py --augment --output_dir "SMB1-unconditional" --num_epochs 500 --json datasets\\SMB1_LevelsAndCaptions-regular-train.json --val_json datasets\\SMB1_LevelsAndCaptions-regular-validate.json
-python run_diffusion.py --model_path SMB1-unconditional --num_samples 100 --save_as_json --output_dir "SMB1-unconditional-samples"
-
-
-
-@echo off
+REM @echo off
 REM Usage: SMB1-ununconditional.bat <type> <seed>
 REM <type> should be "regular" or "absence"
 REM <seed> is optional, defaults to 0
