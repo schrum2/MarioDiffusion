4dec864638a070bd3b6da8965f3c3bec63114e29
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-05-30 13:21:24 -0500

    Combined regular and absence
    #85
    Combined the batch files for Lode Runner conditional regular and absence into one file and deleted the other files

---
+++ batch/LR-conditional.bat
@@ -0,0 +1,21 @@
+
+REM @echo off
+REM Usage: SMB1-conditional.bat <type> <seed>
+REM <type> should be "regular" or "absence"
+REM <seed> is optional, defaults to 0
+cd ..
+
+set TYPE=%1
+if "%TYPE%"=="" set TYPE=regular
+
+set SEED=%2
+if "%SEED%"=="" set SEED=0
+
+set MLM_OUTPUT=LR-MLM-%TYPE%%SEED%
+set DIFF_OUTPUT=LR-conditional-%TYPE%%SEED%
+set UNCOND_OUTPUT=%DIFF_OUTPUT%-unconditional-samples
+
+python train_mlm.py --epochs 300 --save_checkpoints --json datasets\LR_LevelsAndCaptions-%TYPE%-train.json --val_json datasets\LR_LevelsAndCaptions-%TYPE%-validate.json --test_json datasets\LR_LevelsAndCaptions-%TYPE%-test.json --pkl datasets\LR_Tokenizer-%TYPE%.pkl --output_dir %MLM_OUTPUT% --seed %SEED%
+python train_diffusion.py --augment --text_conditional --output_dir "%DIFF_OUTPUT%" --num_epochs 500 --json datasets\LR_LevelsAndCaptions-%TYPE%-train.json --val_json datasets\LR_LevelsAndCaptions-%TYPE%-validate.json --pkl datasets\LR_Tokenizer-%TYPE%.pkl --mlm_model_dir %MLM_OUTPUT% --plot_validation_caption_score --seed %SEED%
+python run_diffusion.py --model_path %DIFF_OUTPUT% --num_samples 100 --text_conditional --save_as_json --output_dir "%UNCOND_OUTPUT%"
+call batch\\LR_evaluate_caption_adherence_multi.bat %DIFF_OUTPUT% %TYPE%
