35ef4fe32c5db0e4e6f86026694358e331d1961e
Jacob Schrum <schrum2@southwestern.edu>
2025-05-30 08:55:08 -0500

    batch prefix needed
    because the scripts start with cd .., this batch file actually is one directory up

--- batch/SMB1-conditional-MiniLM.bat
+++ batch/SMB1-conditional-MiniLM.bat
@@ -1,4 +1,4 @@
-@echo off
+REM @echo off
 REM Usage: SMB1-conditional-MiniLM.bat <type> <seed>
 REM <type> should be "regular" or "absence"
 REM <seed> is optional, defaults to 0
@@ -15,4 +15,4 @@ set UNCOND_OUTPUT=%DIFF_OUTPUT%-unconditional-samples
 
 python train_diffusion.py --augment --text_conditional --output_dir "%DIFF_OUTPUT%" --num_epochs 500 --json datasets\SMB1_LevelsAndCaptions-%TYPE%-train.json --val_json datasets\SMB1_LevelsAndCaptions-%TYPE%-validate.json --pretrained_language_model "sentence-transformers/multi-qa-MiniLM-L6-cos-v1" --plot_validation_caption_score --seed %SEED%
 python run_diffusion.py --model_path %DIFF_OUTPUT% --num_samples 100 --text_conditional --save_as_json --output_dir "%UNCOND_OUTPUT%"
-call evaluate_caption_adherence_multi.bat %DIFF_OUTPUT% %TYPE%
+call batch\\evaluate_caption_adherence_multi.bat %DIFF_OUTPUT% %TYPE%
--- batch/SMB1-conditional-negative.bat
+++ batch/SMB1-conditional-negative.bat
@@ -1,4 +1,4 @@
-@echo off
+REM @echo off
 REM Usage: SMB1-conditional-negative.bat <type> <seed>
 REM <type> should be "regular" or "absence" (should simply be regular)
 REM <seed> is optional, defaults to 0
@@ -17,4 +17,4 @@ set UNCOND_OUTPUT=%DIFF_OUTPUT%-unconditional-samples
 python train_mlm.py --epochs 300 --save_checkpoints --json datasets\SMB1_LevelsAndCaptions-%TYPE%-train.json --val_json datasets\SMB1_LevelsAndCaptions-%TYPE%-validate.json --test_json datasets\SMB1_LevelsAndCaptions-%TYPE%-test.json --pkl datasets\SMB1_Tokenizer-%TYPE%.pkl --output_dir %MLM_OUTPUT% --seed %SEED%
 python train_diffusion.py --augment --text_conditional --output_dir "%DIFF_OUTPUT%" --num_epochs 500 --json datasets\SMB1_LevelsAndCaptions-%TYPE%-train.json --val_json datasets\SMB1_LevelsAndCaptions-%TYPE%-validate.json --pkl datasets\SMB1_Tokenizer-%TYPE%.pkl --mlm_model_dir %MLM_OUTPUT% --plot_validation_caption_score --seed %SEED% --negative_prompt_training
 python run_diffusion.py --model_path %DIFF_OUTPUT% --num_samples 100 --text_conditional --save_as_json --output_dir "%UNCOND_OUTPUT%"
-call evaluate_caption_adherence_multi.bat %DIFF_OUTPUT% %TYPE%
+call batch\\evaluate_caption_adherence_multi.bat %DIFF_OUTPUT% %TYPE%
