9438752cbbac6865618d3fbbc0146964278e63f3
Jacob Schrum <schrum2@southwestern.edu>
2025-05-30 15:12:13 -0500

    combine scripts

--- batch/SMB1-conditional-MiniLM.bat
+++ batch/SMB1-conditional-MiniLM.bat
@@ -1,6 +1,6 @@
 REM @echo off
 REM Usage: SMB1-conditional-MiniLM.bat <type> <seed>
-REM <type> should be "regular" or "absence"
+REM <type> should be "regular", "absence", or "negative"
 REM <seed> is optional, defaults to 0
 cd ..
 
@@ -10,9 +10,16 @@ if "%TYPE%"=="" set TYPE=regular
 set SEED=%2
 if "%SEED%"=="" set SEED=0
 
-set DIFF_OUTPUT=SMB1-conditional-%TYPE%-MiniLM%SEED%
+set DIFF_OUTPUT=SMB1-conditional-MiniLM-%TYPE%%SEED%
+set DIFF_FLAGS=
 set UNCOND_OUTPUT=%DIFF_OUTPUT%-unconditional-samples
 
-python train_diffusion.py --augment --text_conditional --output_dir "%DIFF_OUTPUT%" --num_epochs 500 --json datasets\SMB1_LevelsAndCaptions-%TYPE%-train.json --val_json datasets\SMB1_LevelsAndCaptions-%TYPE%-validate.json --pretrained_language_model "sentence-transformers/multi-qa-MiniLM-L6-cos-v1" --plot_validation_caption_score --seed %SEED%
+REM Special case for negative prompt training
+if /I "%TYPE%"=="negative" (
+    set TYPE=regular
+    set DIFF_FLAGS=--negative_prompt_training
+)
+
+python train_diffusion.py --augment --text_conditional --output_dir "%DIFF_OUTPUT%" --num_epochs 500 --json datasets\SMB1_LevelsAndCaptions-%TYPE%-train.json --val_json datasets\SMB1_LevelsAndCaptions-%TYPE%-validate.json --pretrained_language_model "sentence-transformers/multi-qa-MiniLM-L6-cos-v1" --plot_validation_caption_score --seed %SEED% %DIFF_FLAGS%
 python run_diffusion.py --model_path %DIFF_OUTPUT% --num_samples 100 --text_conditional --save_as_json --output_dir "%UNCOND_OUTPUT%"
 call batch\\evaluate_caption_adherence_multi.bat %DIFF_OUTPUT% %TYPE%
