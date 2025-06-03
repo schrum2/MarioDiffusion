69067597e9c0e1b9fee2096fcd870f7c01157115
schrum2 <schrum2@southwestern.edu>
2025-06-03 09:24:59 -0500

    No need to specify game in filename
    make GAME a parameter instead for for generality

---
+++ batch/train-unconditional.bat
@@ -0,0 +1,16 @@
+REM @echo off
+REM Usage: train-ununconditional.bat <seed> <game>
+REM <seed> is optional, defaults to 0
+REM <game> indicates source of data: SMB1, SMB2, etc.
+cd ..
+
+set SEED=%1
+if "%SEED%"=="" set SEED=0
+
+set GAME=%2
+
+set DIFF_OUTPUT=%GAME%-unconditional%SEED%
+set UNCOND_OUTPUT=%DIFF_OUTPUT%-samples
+
+python train_diffusion.py --augment --output_dir "%DIFF_OUTPUT%" --num_epochs 500 --json datasets\%GAME%_LevelsAndCaptions-regular-train.json --val_json datasets\%GAME%_LevelsAndCaptions-regular-validate.json --seed %SEED%
+call batch\run_diffusion_multi.bat %DIFF_OUTPUT% %TYPE% 
