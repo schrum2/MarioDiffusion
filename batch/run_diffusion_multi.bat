03ca555b25b6a390b2c63da045a860f20c42ed07
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-06-06 15:55:21 -0500

    Added game argument
    #85
    Added game argument so that train and run diffusion knows which game (mario or Lode Runner) it should run on

--- batch/run_diffusion_multi.bat
+++ batch/run_diffusion_multi.bat
@@ -1,11 +1,12 @@
 REM @echo off
-REM Usage: run_diffusion_multi.bat <model_path> <type> <text>
+REM Usage: run_diffusion_multi.bat <model_path> <type> <game> <text>
 REM <type> should be "regular", "absence", or "negative"
 REM This script runs all standard evaluate_caption_adherence.py calls for a given model and type.
 
 set MODEL_PATH=%1
 set TYPE=%2
-set TEXT=%3
+set GAME=%3
+set TEXT=%4
 
 if "%MODEL_PATH%"=="" (
     echo ERROR: Must provide model_path as first argument.
@@ -22,6 +23,9 @@ if /I "%TYPE%"=="absence" set DESCRIBE_ABSENCE_FLAG=--describe_absence
 
 set UNCOND_OUTPUT=%MODEL_PATH%-unconditional-samples
 
-python run_diffusion.py --model_path %MODEL_PATH% --num_samples 100 %TEXT_FLAG% --save_as_json --output_dir "%UNCOND_OUTPUT%-short" %DESCRIBE_ABSENCE_FLAG%
-python run_diffusion.py --model_path %MODEL_PATH% --num_samples 100 %TEXT_FLAG% --save_as_json --output_dir "%UNCOND_OUTPUT%-long" %DESCRIBE_ABSENCE_FLAG% --level_width 128
+set GAME_PLAYED=
+if /I "%GAME%"=="LR" set GAME_PLAYED=--game LR
+
+python run_diffusion.py --model_path %MODEL_PATH% --num_samples 100 %TEXT_FLAG% --save_as_json --output_dir "%UNCOND_OUTPUT%-short" %DESCRIBE_ABSENCE_FLAG% %GAME_PLAYED%
+python run_diffusion.py --model_path %MODEL_PATH% --num_samples 100 %TEXT_FLAG% --save_as_json --output_dir "%UNCOND_OUTPUT%-long" %DESCRIBE_ABSENCE_FLAG% --level_width 128 %GAME_PLAYED%
 
