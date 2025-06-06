f334cc42329e782ce79d331a404a07fe935ac775
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-06-06 15:22:48 -0500

    Deleted num_tile argument
    #85
    Deleted num_tile argument and made it to where depending on the game inputted chooses the number of tiles

--- batch/evaluate_caption_adherence_multi.bat
+++ batch/evaluate_caption_adherence_multi.bat
@@ -7,7 +7,6 @@ REM This script runs all standard evaluate_caption_adherence.py calls for a give
 set MODEL_PATH=%1
 set TYPE=%2
 set GAME=%3
-set NUM_TILES=%4
 
 if "%MODEL_PATH%"=="" (
     echo ERROR: Must provide model_path as first argument.
@@ -15,7 +14,10 @@ if "%MODEL_PATH%"=="" (
 )
 if "%TYPE%"=="" set TYPE=regular
 
-if "%GAME%"=="" set GAME=Mar1and2
+set NUM_TILES=
+
+if /I "%GAME%"=="" set GAME=Mar1and2
+if /I "%GAME%"=="LR" set NUM_TILES=--num_tiles=8
 
 if "%NUM_TILES%"=="" set NUM_TILES=13
 
@@ -23,9 +25,11 @@ REM Add --describe_absence flag if TYPE is absence
 set DESCRIBE_ABSENCE_FLAG=
 if /I "%TYPE%"=="absence" set DESCRIBE_ABSENCE_FLAG=--describe_absence
 
-python evaluate_caption_adherence.py --model_path %MODEL_PATH% --save_as_json --json datasets\%GAME%_LevelsAndCaptions-%TYPE%.json --output_dir samples-from-real-%GAME%-captions --num_tiles %NUM_TILES% %DESCRIBE_ABSENCE_FLAG%
-python evaluate_caption_adherence.py --model_path %MODEL_PATH% --save_as_json --json datasets\%GAME%_LevelsAndCaptions-%TYPE%.json --compare_checkpoints --num_tiles %NUM_TILES% %DESCRIBE_ABSENCE_FLAG%
-python evaluate_caption_adherence.py --model_path %MODEL_PATH% --save_as_json --json datasets\%GAME%_LevelsAndCaptions-%TYPE%-test.json --compare_checkpoints --num_tiles %NUM_TILES% %DESCRIBE_ABSENCE_FLAG%
-python evaluate_caption_adherence.py --model_path %MODEL_PATH% --save_as_json --json datasets\%GAME%_RandomTest-%TYPE%.json --output_dir samples-from-random-%GAME%-captions --num_tiles %NUM_TILES% %DESCRIBE_ABSENCE_FLAG%
-python evaluate_caption_adherence.py --model_path %MODEL_PATH% --save_as_json --json datasets\%GAME%_RandomTest-%TYPE%.json --compare_checkpoints --num_tiles %NUM_TILES% %DESCRIBE_ABSENCE_FLAG%
+
+
+python evaluate_caption_adherence.py --model_path %MODEL_PATH% --save_as_json --json datasets\%GAME%_LevelsAndCaptions-%TYPE%.json --output_dir samples-from-real-%GAME%-captions %NUM_TILES% %DESCRIBE_ABSENCE_FLAG%
+python evaluate_caption_adherence.py --model_path %MODEL_PATH% --save_as_json --json datasets\%GAME%_LevelsAndCaptions-%TYPE%.json --compare_checkpoints %NUM_TILES% %DESCRIBE_ABSENCE_FLAG%
+python evaluate_caption_adherence.py --model_path %MODEL_PATH% --save_as_json --json datasets\%GAME%_LevelsAndCaptions-%TYPE%-test.json --compare_checkpoints %NUM_TILES% %DESCRIBE_ABSENCE_FLAG%
+python evaluate_caption_adherence.py --model_path %MODEL_PATH% --save_as_json --json datasets\%GAME%_RandomTest-%TYPE%.json --output_dir samples-from-random-%GAME%-captions %NUM_TILES% %DESCRIBE_ABSENCE_FLAG%
+python evaluate_caption_adherence.py --model_path %MODEL_PATH% --save_as_json --json datasets\%GAME%_RandomTest-%TYPE%.json --compare_checkpoints %NUM_TILES% %DESCRIBE_ABSENCE_FLAG%
 
