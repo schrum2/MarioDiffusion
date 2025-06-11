8e623db4a2466074b15130baeceed41ccb200a49
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-06-11 16:15:54 -0500

    Added cd
    #76
    Added cd .. so that it goes down a directory to run the files

--- batch/caption_order_tolerance_runner.bat
+++ batch/caption_order_tolerance_runner.bat
@@ -1,3 +1,4 @@
+cd ..
 REM Loop through all directories starting with Mar1and2-conditional in the current directory and run evaluate_caption_order_tolerance
 for /D %%D in ("Mar1and2-conditional*0") do (
     if "absence" in %%D(
@@ -8,4 +9,3 @@ for /D %%D in ("Mar1and2-conditional*0") do (
 
     python evaluate_caption_order_tolerance.py --json datasets\Mar1and2_LevelsAndCaptions-%TYPE%-test.json --game Mario --save_as_json --model_path %%~nxD
 )
-pause
