703daf39e83588ba32bfc2aefd86f2ed335751e0
Bess Hagan <142757968+besshagan@users.noreply.github.com>
2025-06-17 09:16:03 -0500

    Updated naming conventions to plot WGAN and FDM.
    #102

--- util/naming_conventions.py
+++ util/naming_conventions.py
@@ -14,6 +14,11 @@ model_name_map = [
     ("Mar1and2-conditional-GTEsplit-regular", "GTE-multiple-regular"),
     ("Mar1and2-conditional-GTEsplit-absence", "GTE-multiple-absence"),
     ("Mar1and2-conditional-GTEsplit-negative", "GTE-multiple-negative"),
+    ("Mar1and2-fdm-MiniLM-regular", "FDM-MiniLM-regular"),
+    ("Mar1and2-fdm-MiniLM-absence", "FDM-MiniLM-absence"),
+    ("Mar1and2-fdm-GTE-regular", "FDM-GTE-regular"),
+    ("Mar1and2-fdm-GTE-absence", "FDM-GTE-absence"),
+    ("Mar1and2-wgan", "WGAN"),
     ("Mar1and2-unconditional", "Unconditional"),
 ]
 
