9c00b0fb6c92bfd47fc432b3765fce5752e9f951
Bess Hagan <142757968+besshagan@users.noreply.github.com>
2025-06-18 16:48:11 -0500

    Starting a batch file to run A* solvability on everything in G-drive
    #109

--- batch/Mar1and2-conditional-evaluate-solvability.bat
+++ batch/Mar1and2-conditional-evaluate-solvability.bat
@@ -1,8 +1,23 @@
 @echo off
-
 REM Loop through all directories starting with Mar1and2-conditional in the current directory
 for /D %%D in ("Mar1and2-conditional*0") do (
     echo Processing %%D
     python evaluate_solvability.py --num_runs 1 --model_path "%%D"
 )
 pause
+
+:: Unconditional Models
+python evaluate_solvability.py --num_runs 1 --model_path "Mar1and2-unconditional0"
+
+:: FDM Models
+python evaluate_solvability.py --num_runs 1 --model_path "Mar1and2-fdm-MiniLM-regular0"
+python evaluate_solvability.py --num_runs 1 --model_path "Mar1and2-fdm-MiniLM-absence0"
+python evaluate_solvability.py --num_runs 1 --model_path "Mar1and2-fdm-GTE-regular0"
+python evaluate_solvability.py --num_runs 1 --model_path "Mar1and2-fdm-GTE-absence0"
+
+:: GTE Models
+python evaluate_solvability.py --num_runs 1 --model_path "Mar1and2-conditional-GTEsplit-negative0"
+
+:: WGAN Models : Mar1and2-wgan0-samples (evaluate_solvability.py can't handle this, temp script in google drive)
+
+:: MarioGPT Models : Ask Olivia
