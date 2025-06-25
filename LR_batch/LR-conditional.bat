5dddbe11528abc10cb1c1a3e1ac5086dc3273ea4
williamsr03 <156847388+williamsr03@users.noreply.github.com>
2025-06-25 10:49:13 -0500

    Changed LR batch file
    #106 #116
    Changed a line in a batch file so that evaluate_caption_adherence_multi.bat can be ran for Lode Runner

--- LR_batch/LR-conditional.bat
+++ LR_batch/LR-conditional.bat
@@ -14,8 +14,9 @@ if "%SEED%"=="" set SEED=0
 set MLM_OUTPUT=LR-MLM-%TYPE%%SEED%
 set DIFF_OUTPUT=LR-conditional-%TYPE%%SEED%
 set UNCOND_OUTPUT=%DIFF_OUTPUT%-unconditional-samples
+set GAME=LR
 
 python train_mlm.py --epochs 300 --save_checkpoints --json datasets\LR_LevelsAndCaptions-%TYPE%-train.json --val_json datasets\LR_LevelsAndCaptions-%TYPE%-validate.json --test_json datasets\LR_LevelsAndCaptions-%TYPE%-test.json --pkl datasets\LR_Tokenizer-%TYPE%.pkl --output_dir %MLM_OUTPUT% --seed %SEED%
 python train_diffusion.py --augment --text_conditional --output_dir "%DIFF_OUTPUT%" --num_epochs 500 --json datasets\LR_LevelsAndCaptions-%TYPE%-train.json --val_json datasets\LR_LevelsAndCaptions-%TYPE%-validate.json --pkl datasets\LR_Tokenizer-%TYPE%.pkl --mlm_model_dir %MLM_OUTPUT% --plot_validation_caption_score --seed %SEED%
 python run_diffusion.py --model_path %DIFF_OUTPUT% --num_samples 100 --text_conditional --save_as_json --output_dir "%UNCOND_OUTPUT%"
-call batch\\LR_evaluate_caption_adherence_multi.bat %DIFF_OUTPUT% %TYPE%
+call batch\\evaluate_caption_adherence_multi.bat %DIFF_OUTPUT% %TYPE% %GAME%
