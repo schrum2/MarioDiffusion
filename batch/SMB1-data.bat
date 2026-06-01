a6769bfdf8dbe5343e0b3ad34b92d7ca96d19b72
Fletcher Wilson <145033473+fwilson12@users.noreply.github.com>
2026-06-01 14:12:15 -0500

    (#96) Refactor filename conventions for variably-sized dsets
    Standardized the filename system when creating datasets of levels of specified width, appending the width after the game name in each new file, e.g., Mar1and2_64_*.
    
    When calling a script that uses these datasets, the --game arg should be adapted to include the width suffix, e.g., --game SMB2_128

--- batch/SMB1-data.bat
+++ batch/SMB1-data.bat
@@ -1,7 +1,9 @@
 @echo off
 cd ..
 
-if not exist "datasets" mkdir datasetset WIDTH=%1
+if not exist "datasets" mkdir datasetset 
+
+set WIDTH=%1
 set WIDTH_SUFFIX=
 set WIDTH_ARG=
 if not "%WIDTH%"=="" (
@@ -9,24 +11,23 @@ if not "%WIDTH%"=="" (
     set WIDTH_ARG=--target_width %WIDTH%
 )
 
-set default_out=datasets\SMB1_LevelsAndCaptions%WIDTH_SUFFIX%
+set default_out=datasets\SMB1%WIDTH_SUFFIX%_LevelsAndCaptions
 
 :: Convert SMB1 raw level data to JSON
-python create_level_json_data.py --output "datasets\\SMB1_Levels%WIDTH_SUFFIX%.json" --levels "..\\TheVGLC\\Super Mario Bros\\Processed" %WIDTH_ARG%
+python create_level_json_data.py --output "datasets\\SMB1%WIDTH_SUFFIX%_Levels.json" --levels "..\\TheVGLC\\Super Mario Bros\\Processed" %WIDTH_ARG%
 
 :: Generate captions for SMB1
-python create_ascii_captions.py --dataset datasets\\SMB1_Levels%WIDTH_SUFFIX%.json --output %default_out%-regular.json --exclude_upside_down_pipes
-python create_ascii_captions.py --dataset datasets\\SMB1_Levels%WIDTH_SUFFIX%.json --output %default_out%-absence.json --exclude_upside_down_pipes --describe_absence
+python create_ascii_captions.py --dataset datasets\\SMB1%WIDTH_SUFFIX%_Levels.json --output %default_out%-regular.json --exclude_upside_down_pipes
+python create_ascii_captions.py --dataset datasets\\SMB1%WIDTH_SUFFIX%_Levels.json --output %default_out%-absence.json --exclude_upside_down_pipes --describe_absence
 
 :: Tokenize SMB1 data
-python tokenizer.py save --json_file %default_out%-regular.json --pkl_file datasets\\SMB1_Tokenizer-regular%WIDTH_SUFFIX%.pkl
-python tokenizer.py save --json_file %default_out%-absence.json --pkl_file datasets\\SMB1_Tokenizer-absence%WIDTH_SUFFIX%.pkl
+python tokenizer.py save --json_file %default_out%-regular.json --pkl_file datasets\\SMB1%WIDTH_SUFFIX%_Tokenizer-regular.pkl
+python tokenizer.py save --json_file %default_out%-absence.json --pkl_file datasets\\SMB1%WIDTH_SUFFIX%_Tokenizer-absence.pkl
 
 :: Create validation captions for SMB1 dataset, using the previously generated JSON files
 :: Added the --no_upside_down_pipes flag to SMB1 validation captions
-python create_random_test_captions.py --save_file "datasets\\SMB1_RandomTest-regular%WIDTH_SUFFIX%.json" --json %default_out%-regular.json --seed 0 --no_upside_down_pipes
-python create_random_test_captions.py --save_file "datasets\\SMB1_RandomTest-absence%WIDTH_SUFFIX%.json" --json %default_out%-absence.json --seed 0 --describe_absence --no_upside_down_pipes
-no_upside_down_pipes
+python create_random_test_captions.py --save_file "datasets\\SMB1%WIDTH_SUFFIX%_RandomTest-regular.json" --json %default_out%-regular.json --seed 0 --no_upside_down_pipes
+python create_random_test_captions.py --save_file "datasets\\SMB1%WIDTH_SUFFIX%_RandomTest-absence.json" --json %default_out%-absence.json --seed 0 --describe_absence --no_upside_down_pipes 
 
 :: Split output files into train/val/test sets
 python split_data.py --json %default_out%-regular.json --train_pct 0.9 --val_pct 0.05 --test_pct 0.05 --seed 0 --game mario
