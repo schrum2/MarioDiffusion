dca2e1880063d22a78718b004a97db0b873a643e
almzayena-lab <almzayena@southwestern.edu>
2026-06-11 10:04:53 -0500

    Update MegaManMaker.bat

--- batch/MegaManMaker.bat
+++ batch/MegaManMaker.bat
@@ -1,14 +1,11 @@
 @echo off
 cd ..
-set MMM=%LOCALAPPDATA%\MegaMaker\Levels
 set /p FILE=Upload a file: 
 
 if "%FILE:~-5%"==".mmlv" (
-    python -m megaman.mmlv_to_vglc "%FILE%" levels\output.txt
-    echo Saved to levels\output.txt
+    python -m megaman.mmlv_to_vglc "%FILE%"
 ) else if "%FILE:~-4%"==".txt" (
-    python -m megaman.vglc_to_mmlv "%FILE%" "%MMM%\output.mmlv"
-    echo Saved to %MMM%\output.mmlv
+    python -m megaman.vglc_to_mmlv "%FILE%"
 ) else (
     echo Unsupported file type. Please provide a .mmlv or .txt file.
 )
