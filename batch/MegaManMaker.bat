ac59150d9692a88c00a1addc9e2f6c79c1f12322
almzayena-lab <almzayena@southwestern.edu>
2026-06-11 13:43:29 -0500

    Batch file updates
    #165

--- batch/MegaManMaker.bat
+++ batch/MegaManMaker.bat
@@ -1,11 +1,52 @@
 @echo off
-cd ..
-set /p FILE=Upload a file: 
 
-if "%FILE:~-5%"==".mmlv" (
-    python -m megaman.mmlv_to_vglc "%FILE%"
-) else if "%FILE:~-4%"==".txt" (
-    python -m megaman.vglc_to_mmlv "%FILE%"
-) else (
-    echo Unsupported file type. Please provide a .mmlv or .txt file.
+setlocal enabledelayedexpansion
+
+set MMM=%USERPROFILE%\AppData\Local\MegaMaker\Levels
+set VGLC=%USERPROFILE%\Documents\GitHub\TheVGLC\MegaMan\Enhanced
+set CONDA_PYTHON=%USERPROFILE%\AppData\Local\miniconda3\envs\SURF\python.exe
+cd /d "%~dp0.."
+
+set INTERACTIVE=1
+if not "%~1"=="" (
+    set INTERACTIVE=0
+    set FILE=%~1
+    call :GETINFO "%~1"
+    if "!EXT!"==".mmlv" goto DOMMLV
+    if "!EXT!"==".txt" goto DOTXT
+    echo Unsupported file type.
+    goto END
 )
+
+:LOOP
+set FILE=
+set /p FILE=Upload a file (or type Q to quit): 
+
+if /i "!FILE!"=="q" goto END
+
+call :GETINFO "!FILE!"
+
+if "!EXT!"==".mmlv" goto DOMMLV
+if "!EXT!"==".txt" goto DOTXT
+echo Unsupported file type. Please provide a .mmlv or .txt file.
+goto LOOP
+
+:DOMMLV
+"!CONDA_PYTHON!" -m megaman.mmlv_to_vglc "!FILE!"
+copy "!DIR!!NAME!.txt" "%VGLC%\"
+if "!INTERACTIVE!"=="0" goto END
+goto LOOP
+
+:DOTXT
+"!CONDA_PYTHON!" -m megaman.vglc_to_mmlv "!FILE!" "!DIR!!NAME!.mmlv"
+copy "!DIR!!NAME!.mmlv" "%MMM%\"
+if "!INTERACTIVE!"=="0" goto END
+goto LOOP
+
+:GETINFO
+set NAME=%~n1
+set DIR=%~dp1
+set EXT=%~x1
+exit /b
+
+:END
