35088730d98407103a6e78974a9a38fa5cab5af7
patwikk <160638416+patwikk@users.noreply.github.com>
2026-07-09 16:39:46 -0500

    Create extract_10k_10likes.bat

---
+++ MM2_Batch/extract_10k_10likes.bat
@@ -0,0 +1,7 @@
+cd ..
+python -m mm2pipeline_data extract --output_folder MM2_Data\bcd --limit 10000 --skip_3dworld --skip_items --skip_subworld_items --likes 10
+python -m mm2pipeline_data toost --input MM2_Data\bcd -o MM2_Data\json --images-output MM2_Data\images
+python -m mm2pipeline_data json-to-ascii --input MM2_Data\json --output_folder MM2_Data\ascii
+python -m mm2pipeline_data dataset build --input MM2_Data\ascii --output_folder MM2_Data\dataset\dataset.json --tileset mm2_tileset_we.json --sliding_window --stride 20
+python -m mm2pipeline_data dataset split --input MM2_Data\dataset\dataset.json --seed 0
+
