07f8a9ca2e96d78daa5cdd601c3779c7a6a4c468
patwikk <160638416+patwikk@users.noreply.github.com>
2026-07-10 15:42:25 -0500

    Update extract_25k_10likes.bat
    Updated to do the full pipeline.

--- MM2_Batch/extract_25k_10likes.bat
+++ MM2_Batch/extract_25k_10likes.bat
@@ -20,3 +20,24 @@ python tokenizer.py save --json_file datasets\MM2_LevelsAndCaptions-regular.json
 python create_random_test_captions.py --save_file datasets\MM2_RandomTest-regular.json --json datasets\MM2_LevelsAndCaptions-regular.json --seed %SEED% --game MM2
 
 python split_data.py --json_file datasets\MM2_LevelsAndCaptions-regular.json --train_pct 0.9 --val_pct 0.05 --test_pct 0.05 --seed 42 --game MM2
+
+set GAME=MM2
+set NUM_TILES=67
+set JSON_TRAIN=datasets\MM2_LevelsAndCaptions-regular-train.json
+set JSON_VAL=datasets\MM2_LevelsAndCaptions-regular-validate.json
+set JSON_TEST=datasets\MM2_LevelsAndCaptions-regular-test.json
+set JSON_RANDOM=datasets\MM2_RandomTest-regular.json
+set PKL=datasets\MM2_Tokenizer-regular.pkl
+set MLM_OUTPUT=MM2-MLM-regular%SEED%
+set DIFF_OUTPUT=MM2-conditional-regular%SEED%
+
+python train_mlm.py --epochs 300 --save_checkpoints --json %JSON_TRAIN% --val_json %JSON_VAL% --test_json %JSON_TEST% --pkl %PKL% --output_dir %MLM_OUTPUT% --seed %SEED%
+
+python train_diffusion.py --save_image_epochs 1000 --augment --text_conditional --output_dir %DIFF_OUTPUT% --num_epochs 500 --json %JSON_TRAIN% --val_json %JSON_VAL% --pkl %PKL% --mlm_model_dir %MLM_OUTPUT% --plot_validation_caption_score --seed %SEED% --game %GAME%
+
+python run_diffusion.py --model_path %DIFF_OUTPUT% --num_samples 100 --text_conditional --save_as_json --output_dir "%DIFF_OUTPUT%-samples" --game %GAME%
+
+python evaluate_caption_adherence.py --model_path %DIFF_OUTPUT% --save_as_json --json %JSON_TEST% --compare_checkpoints --num_tiles %NUM_TILES% --game %GAME%
+python evaluate_caption_adherence.py --model_path %DIFF_OUTPUT% --save_as_json --json %JSON_RANDOM% --output_dir "%DIFF_OUTPUT%-caption-adherence-random" --num_tiles %NUM_TILES% --game %GAME%
+
+python -m MM2_Files.evaluate_mm2_metrics --model_path %DIFF_OUTPUT% --game %GAME%
