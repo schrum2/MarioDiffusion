48460141ffa1a7e085b22573675be1ebb4f5f095
schrum2 <schrum2@southwestern.edu>
2026-07-26 00:57:08 -0500

    Not testing embeddings any more
    Purpose was to see how well different embedding lengths worked with the two embedding approaches, but ultimately not that important for us.

--- batch/CHECK_BLOCK_EMBEDDINGS_Mario.bat
+++
@@ -1,8 +0,0 @@
-call train-unconditional-embeddings.bat 0 block2vec 16
-call train-unconditional-embeddings.bat 0 block2vec 8
-call train-unconditional-embeddings.bat 0 skip 16
-call train-unconditional-embeddings.bat 0 skip 8
-call train-conditional-embeddings.bat 0 block2vec 16
-call train-conditional-embeddings.bat 0 block2vec 8
-call train-conditional-embeddings.bat 0 skip 16
-call train-conditional-embeddings.bat 0 skip 8
