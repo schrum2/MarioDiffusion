fc4b7f0d3258026313f0a9027db6e6d022ae7fb5
schrum2 <schrum2@southwestern.edu>
2026-09-06 23:41:36 -0500

    Focus on Qwen now

--- batch/TODO.bat
+++ batch/TODO.bat
@@ -1,5 +1,5 @@
-call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 1 no no gemma4:12b_captions
-call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 2 no no gemma4:12b_captions
-call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 3 no no gemma4:12b_captions
-call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 4 no no gemma4:12b_captions
-call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 5 no no gemma4:12b_captions
+call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 300 1 no no qwen3.5:9b_captions
+REM call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 2 no no qwen3.5:9b_captions
+call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 300 3 no no qwen3.5:9b_captions
+REM call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 4 no no qwen3.5:9b_captions
+call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 300 5 no no qwen3.5:9b_captions
