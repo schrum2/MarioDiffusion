0c66f27f8d453b7a26656cbdcb21af0fac19f913
schrum2 <schrum2@southwestern.edu>
2026-09-11 07:22:00 -0500

    Change

--- batch/TODO.bat
+++ batch/TODO.bat
@@ -1,5 +1,24 @@
-call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 300 1 no no qwen3.5:9b_captions
-REM call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 2 no no qwen3.5:9b_captions
-call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 300 3 no no qwen3.5:9b_captions
-REM call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 4 no no qwen3.5:9b_captions
-call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 300 5 no no qwen3.5:9b_captions
+
+
+
+call train-diffusion.bat 1 MM2 llm MM2 CLIP single none 0 200 1 no no gemma4:12b_captions
+REM call train-diffusion.bat 1 MM2 llm MM2 CLIP single none 0 200 2 no no gemma4:12b_captions
+call train-diffusion.bat 1 MM2 llm MM2 CLIP single none 0 200 3 no no gemma4:12b_captions
+REM call train-diffusion.bat 1 MM2 llm MM2 CLIP single none 0 200 4 no no gemma4:12b_captions
+call train-diffusion.bat 1 MM2 llm MM2 CLIP single none 0 200 5 no no gemma4:12b_captions
+
+
+
+
+
+
+
+
+
+
+
+REM call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 1 no no gemma4:12b_captions
+REM call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 2 no no gemma4:12b_captions
+REM call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 3 no no gemma4:12b_captions
+REM call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 4 no no gemma4:12b_captions
+REM call train-diffusion.bat 1 MMLV llm MMLV CLIP single none 0 200 5 no no gemma4:12b_captions
